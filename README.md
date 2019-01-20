68d7eccc540bbe2dab816c23dde5c5abc2ea37e9
Glitch (node-file-json) <none>
2019-01-20 10:35:48 +0000

    🛍💪 Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # node-file-json
 Read JSON from a file and work on it dynamically. You can even use the JSON file as a small database. If file does not exist, one will be created for you.
+
+**Usage:**
+
+Load JSON file, if it does not exist, it will be created automatically.
+```
+var json = require("node-file-json");
+json.load("product.json");
+```
+
+Modify JSON data
+```
+json.data.property1 = "First property";
+json.data.property2 = {name: "Product name", details: "These are some details."};
+```
+
+Save JSON data
+
+```
+json.save();
+```
+
+Data will be saved in the file as defined in .load() to be read any time.<br/>
+If any errors occur during loading/creating JSON file, check console log.
+JSON in file is saved properly with indents. To modify indent value, add it to options, example:
+```
+json.load("product.json", {indent: 4});
+```
---
+++ main.js
@@ -0,0 +1,32 @@
+var fs = require("fs");
+var sanitize = require("node-sanitize-options");
+var loadedFile;
+var options = {};
+exports.data = {};
+exports.load = function(file, _options) {
+  options = sanitize.options(_options, {indent: 2});
+  if (!fs.existsSync(file)) {
+    exports.data = {};
+    exports.save(file);
+  }
+  if (fs.existsSync(file)) {
+    try {
+      exports.data = JSON.parse(fs.readFileSync(file, "utf-8"));
+      loadedFile = file;
+    } catch(error) {
+      console.log(error);
+    }
+  } else {
+    console.log("Could not load the JSON file: " + file);
+  }
+};
+exports.save = function(file) {
+  if (typeof file === "undefined") file = loadedFile;
+  if (typeof loadedFile === "undefined") {
+    console.log("No JSON file loaded. Please use .load() first.");
+    return false;
+  }
+  var stream = fs.createWriteStream(file);
+  stream.write(JSON.stringify(exports.data, null, options.indent));
+  stream.end();
+};
