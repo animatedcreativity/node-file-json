9c9c6ee4a72a181eadb04f69d69241ec0500a2f4
Glitch (node-file-json) <none>
2019-01-20 14:40:50 +0000

    🐓🌸 Updated with Glitch

--- main.js
+++ main.js
@@ -1,30 +1,35 @@
-var fs = require("fs");
-var sanitize = require("node-sanitize-options");
-var loadedFile;
-var options = {};
-exports.data = {};
-exports.load = function(file, _options) {
-  options = sanitize.options(_options, {indent: 2});
-  if (!fs.existsSync(file)) {
-    exports.data = {};
-    exports.save(file);
-  }
-  if (fs.existsSync(file)) {
-    try {
-      exports.data = JSON.parse(fs.readFileSync(file, "utf-8"));
-      loadedFile = file;
-    } catch(error) {
-      console.log(error);
+exports.fileJson = function() {
+  var fs = require("fs");
+  var sanitize = require("node-sanitize-options");
+  var loadedFile;
+  var options = {};
+  var json = {
+    data: {},
+    load: function(file, _options) {
+      options = sanitize.options(_options, {indent: 2});
+      if (!fs.existsSync(file)) {
+        json.data = {};
+        json.save(file);
+      }
+      if (fs.existsSync(file)) {
+        try {
+          json.data = JSON.parse(fs.readFileSync(file, "utf-8"));
+          loadedFile = file;
+        } catch(error) {
+          console.log(error);
+        }
+      } else {
+        console.log("Could not load the JSON file: " + file);
+      }
+    },
+    save: function(file) {
+      if (typeof file === "undefined") file = loadedFile;
+      if (typeof file === "undefined") {
+        console.log("No JSON file loaded. Please use .load() first.");
+        return false;
+      }
+      fs.writeFileSync(file, JSON.stringify(json.data, null, options.indent), "utf-8");
     }
-  } else {
-    console.log("Could not load the JSON file: " + file);
   }
-};
-exports.save = function(file) {
-  if (typeof file === "undefined") file = loadedFile;
-  if (typeof file === "undefined") {
-    console.log("No JSON file loaded. Please use .load() first.");
-    return false;
-  }
-  fs.writeFileSync(file, JSON.stringify(exports.data, null, options.indent), "utf-8");
-};
+  return json;
+}
